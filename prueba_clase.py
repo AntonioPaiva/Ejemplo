a3dbcc1d41ab98ae2b11ba6bf97040ea83a64dd1
AntonioPaiva <paivatooresantonio@gmail.com>
2015-07-16 14:54:46 -0400

    primer commit prueba clase

--- prueba_clase.py
+++ prueba_clase.py
@@ -2,6 +2,9 @@ class ejemplo(object):
     
     def imprimir(self):
         print("Python github")
+        
+    def primer_commit(self):
+    	print("Primer commit, github")
    
 if __name__ == '__main__':
     p = ejemplo()
